3c6b434f2b65f46042a1a95552470797240349a0
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-21 19:07:57 +0545

    changes in models.py

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # Don't create table in DB
+
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Tag(TimeStampModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(TimeStampModel):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("in_active", "Inactive"),
+    ]
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to="post_images/%Y/%m/%d", blank=False)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
+    views_count = models.PositiveBigIntegerField(default=0)
+    published_at = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
